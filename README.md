12ee119ecda6bb1903994d4f9e249e2468191a57
JFernando122 <fersi132@gmail.com>
2021-03-12 12:46:28 -0600

    agregadas nuevas funcionalidades

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Pop - up candy.
 
-Este proyecto esta insiparado de una idea que vi en un grupo de progamacíon. Lo que se pediá era:
+Este proyecto esta inspirado de una idea que vi en un grupo de progamacíon. Lo que se pediá era:
 
 ## Requisitos.
 
-1. Hacer que un dulce (Los que estan el las imágenes) aparezca en intervalos en la pantalla.
+1. Hacer que un dulce (Los que estan en las imágenes) aparezca en intervalos en la pantalla.
 2. El dulce puede aparecer en cualquier parte.
 3. El dulce tiene que aparecer durante un intervalo de tiempo especifico.
 4. El dulce no debe interferir con las demas funcionalidades de la página.
@@ -14,5 +14,30 @@ Este proyecto esta insiparado de una idea que vi en un grupo de progamacíon. Lo
 1. El dulce puede aparecer rotado para agregar mas variedad.
 2. Se debe de hacer de manera modular, es decir poder tener múltiples dulce que funcionen de manera independiente.
 3. Hacer que el intervalo de tiempo pueda variar a conveniencia.
+4. poder modificar el tamaño del dulce a cenveniencia.
 
-El proyecto aun no se encuentra terminado, debido a que aun no se cumplen 2 de los requisitos extra.
+# V2.0
+
+1. Se refactorizó el código a un enfoque POO, para hacerlo modular.
+2. Se agregó que el tiempo en que aparece y desaparece el dulce puede ser asignado a cada dulce de manera independiente.
+3. Se agregó la posibilidad de tener un desfase para que los dulces no aparezcan de manera inmediata.
+4. Se agregó la posibilidad de cambiar el tamaño del dulce a conveniencia (Tendra el mismo ancho y alto, y siempre será redondo)
+5. Se agregaron valores por defecto a los tiempos de aparición desaparición desfase y tamaño del dulce.
+
+El proyecto ahora cumple con todos los requisitos establecidos y requisitos extra.
+
+# Modo de uso
+
+**Solo es necesario el archivo "app.js"** los archivos html y css son solo utilizados para la demostración
+
+Para poder crear un nuevo dulce solo es necesario instanciar una nueva clase especificando el contenedor en el que se van a encontrar los dulces, y el nombre de las imagenes que tendrán los dulces.
+
+El contenedor deberá de tener el atributo ` position: relative ` para poder funcionar apropiadamente
+
+Las imagenes para los dulces deberán estar en la carpeta **candy** y deberán ser formato **.jpg** (*Posiblemente se modifique esto mas adelante*) y solo deberá proporcionarse el nombre de la imágen. Es decir, si la imágen es `candy/dulce1.jpg` solo deberá colocarse `dulce1`
+
+    `new Candy(container, "nombre1", "nombre2", "nombren")`
+
+De igual manera se puede proporcionar el tiempo por el que aparecerá el dulce (apperarTime), el tiempo en el que el dulce no aparecerá en pantalla (disapperar), el tiempo inicial que tomará el dulce para aparecer por primera vez (offset) y por último el tamaño del dulce (size)
+
+    `new Candy(container, {appearTime, disappearTime, offset}, { size }, "nombre1", "nombre2", "nombren")`
--- app.js
+++ app.js
@@ -1,36 +1,64 @@
-const totalCandy = 3
-const wrapper = document.querySelector("#wrapper")
-
-const getCandy = () =>{
-    const imageName = "candy/" + Math.floor(Math.random()*totalCandy+1) + ".jpg"
-    const imageNode = document.createElement("img")
-    imageNode.src = imageName
-    imageNode.classList.add("candy")
-    const top = Math.floor(Math.random()*96) + "vh"
-    const left = Math.floor(Math.random()*96) + "vw"
-    const rotation = Math.floor(Math.random()*180 - 90) + "deg"
-    imageNode.style.top = top
-    imageNode.style.left = left
-    imageNode.style.transform = "rotate( " + rotation + ")"
-    return imageNode
-}
-
-const addCandy = (candy) =>{
-    wrapper.appendChild(candy)
-}
-
-const removeCandy = (candy) =>{
-    wrapper.removeChild(candy)
-}
-
-let candy = null;
-
-setInterval(()=>{
-    if(!candy){
-        candy = getCandy()
-        addCandy(candy)
-    }else{
-        removeCandy(candy)
-        candy = null
+class Candy{
+    constructor(container,{appearTime = 2500,disappearTime= 2500, offset = 0},{size = "100px"}, ...images){
+        if(!container)
+            throw "Please provide a container for the candy"
+        if(images.length === 0)
+            throw "You need to provide the images for the candy in an array"
+        this.container = container
+        this.candy = null
+        this.appearTime = appearTime
+        this.disappearTime = disappearTime,
+        this.images = images
+        this.size = size
+        setTimeout(()=>{
+            this.addCandy(this.candy)
+        },offset)
     }
-},2500)
+    
+    getImageName(){
+        const number = Math.floor(Math.random()*this.images.length)
+        return this.images[number]
+    }
+
+    makeCandy(){
+        const imageName = `candy/${this.getImageName()}.jpg`
+        const imageNode = document.createElement("img")
+        imageNode.src = imageName
+        const top = Math.floor(Math.random()*96) + "vh"
+        const left = Math.floor(Math.random()*96) + "vw"
+        const rotation = Math.floor(Math.random()*180 - 90) + "deg"
+        imageNode.style.width = this.size
+        imageNode.style.height = this.size
+        imageNode.style.borderRadius = "50%"
+        imageNode.style.position = "absolute"
+        imageNode.style.top = top
+        imageNode.style.left = left
+        imageNode.style.transform = "rotate( " + rotation + ")"
+        return imageNode
+    }
+    
+    addCandy(){
+        this.candy = this.makeCandy()
+        this.container.appendChild(this.candy)
+        setTimeout(()=>{
+            this.removeCandy()
+        },this.appearTime)
+    }
+    
+    removeCandy(){
+        this.container.removeChild(this.candy)
+        this.candy = null
+        setTimeout(() =>{
+            this.addCandy()
+        },this.disappearTime)
+    }
+}
+
+new Candy(document.querySelector("#wrapper"),{
+    appearTime:5000,
+    disappearTime: 10000,
+    offset: 3000},
+    {size : "50px"},
+    "1","2","3")
+
+new Candy(document.querySelector("#wrapper"),"1","2","3")
